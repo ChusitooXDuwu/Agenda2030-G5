4b56b1e0dd768c37240e1a3f258d6fbaaf0fff5e
Wyo Hann Chu <w.chu@uniandes.edu.co>
2023-10-18 17:29:17 -0500

    commmit del intento con FASTApi

---
+++ Etapa 1 copy/preprocessing.py
@@ -0,0 +1,22 @@
+from sklearn.base import BaseEstimator, TransformerMixin
+import re
+import ftfy
+
+class Preprocessing  (BaseEstimator, TransformerMixin):
+  def fit(self, X, y=None):
+      return self
+
+  def remove_punctuation(self, text):
+      # Use ftfy to fix text and remove punctuation points using regular expression
+      cleaned_text = ftfy.fix_text(text)
+      cleaned_text = re.sub(r'[^\w\s]', '', cleaned_text)
+      return cleaned_text
+
+  def transform (self, X):
+      X['Textos_espanol'] = X['Textos_espanol'].apply(ftfy.fix_text)
+      X['Textos_espanol'] = X['Textos_espanol'].apply(lambda x: x.lower())
+      # Apply remove_punctuation function to 'Textos_espanol' column
+      X['Textos_espanol'] = X['Textos_espanol'].apply(self.remove_punctuation)
+      X["Textos_espanol"] = X["Textos_espanol"].astype(str)
+
+      return X["Textos_espanol"]
