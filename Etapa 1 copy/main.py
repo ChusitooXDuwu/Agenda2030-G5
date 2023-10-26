331e1b719522ad4f12296fc254ff7bcb1be7dc1b
Wyo Hann Chu <w.chu@uniandes.edu.co>
2023-10-26 14:10:52 -0500

    `commit carga lista, to fix FASTAPI

--- Etapa 1 copy/main.py
+++ Etapa 1 copy/main.py
@@ -37,24 +37,24 @@ import ftfy
 #     model = load("model.joblib")
 #     result = model.predict(df)
 #     return result
-class Preprocessing  (BaseEstimator, TransformerMixin):
-        def fit(self, X, y=None):
-            return self
+# class Preprocessing  (BaseEstimator, TransformerMixin):
+#         def fit(self, X, y=None):
+#             return self
 
-        def remove_punctuation(self, text):
-            # Use ftfy to fix text and remove punctuation points using regular expression
-            cleaned_text = ftfy.fix_text(text)
-            cleaned_text = re.sub(r'[^\w\s]', '', cleaned_text)
-            return cleaned_text
+#         def remove_punctuation(self, text):
+#             # Use ftfy to fix text and remove punctuation points using regular expression
+#             cleaned_text = ftfy.fix_text(text)
+#             cleaned_text = re.sub(r'[^\w\s]', '', cleaned_text)
+#             return cleaned_text
 
-        def transform (self, X):
-            X['Textos_espanol'] = X['Textos_espanol'].apply(ftfy.fix_text)
-            X['Textos_espanol'] = X['Textos_espanol'].apply(lambda x: x.lower())
+#         def transform (self, X):
+#             X['Textos_espanol'] = X['Textos_espanol'].apply(ftfy.fix_text)
+#             X['Textos_espanol'] = X['Textos_espanol'].apply(lambda x: x.lower())
           
-            X['Textos_espanol'] = X['Textos_espanol'].apply(self.remove_punctuation)
-            X["Textos_espanol"] = X["Textos_espanol"].astype(str)
+#             X['Textos_espanol'] = X['Textos_espanol'].apply(self.remove_punctuation)
+#             X["Textos_espanol"] = X["Textos_espanol"].astype(str)
 
-            return X["Textos_espanol"]
+#             return X["Textos_espanol"]
 
 from typing import Optional
 
@@ -79,7 +79,35 @@ def make_predictions(dataModel: Model):
     
     df = pd.DataFrame(dataModel.dict(), columns=dataModel.dict().keys(), index=[0])
     
-    df.columns = dataModel.columns()
-    model = load("model.joblib")
-    result = model.predict(df)
-    return result
+
+    df['Textos_espanol'] = df['Textos_espanol'].apply(ftfy.fix_text)
+
+    def remove_punctuation(text):
+        # Use ftfy to fix text and remove punctuation points using regular expression
+        cleaned_text = ftfy.fix_text(text)
+        cleaned_text = re.sub(r'[^\w\s]', '', cleaned_text)
+        return cleaned_text
+    df['Textos_espanol'] = df['Textos_espanol'].apply(lambda x: x.lower())
+
+    # Apply remove_punctuation function to 'Textos_espanol' column
+    df['Textos_espanol'] = df['Textos_espanol'].apply(remove_punctuation)
+
+
+
+
+
+
+
+
+   
+   #  filename = 'model.joblib'
+   #  df.columns = dataModel.columns()
+   #  # print(df.columns)
+   #  model = load(filename)
+   #  result = model.predict(df)
+
+   #  df["sdg"] = result
+   #  #df.to_csv("/content/sample_data/resultado_prediccion.csv", index=False)
+   #  print(df)
+    return "df"
+
